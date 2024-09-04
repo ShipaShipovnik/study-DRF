9470b35afba38c0aa709a2e48ae3a110bd7e2874
ShipaShipovnik <shipashipovnik2020d@gmail.com>
2024-09-04 10:11:09 +0300

    baza dannyh

--- drfsite/women/models.py
+++ drfsite/women/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_on_add=True)
+    time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT,null=True)
