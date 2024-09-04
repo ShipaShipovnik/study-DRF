70425f27aff310f3d2260d3b083004af9637af70
ShipaShipovnik <shipovnik2020d@gmail.com>
2024-09-04 14:01:39 +0300

    post het put

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'women',
-
-
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.forms.models import model_to_dict
 
-# Create your views here.
+class WomenAPIView(generics.ListAPIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+        # lst = Women.objects.all() # .values()
+        # return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'post': model_to_dict(post_new)})
+
+class WomenAPIList(generics. ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
